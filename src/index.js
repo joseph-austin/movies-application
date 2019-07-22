62de12d307517c0dad4bc8cb4dee1b78792e6a8d
JustinSOrtega <justinsortega@gmail.com>
2019-07-22 09:00:52 -0500

    class started, text desplayed

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
  * es6 modules and imports
  */
 import sayHello from './hello';
+
 sayHello('World');
 
 /**
@@ -9,12 +10,35 @@ sayHello('World');
  */
 import getMovies from './api.js';
 
-getMovies().then((movies) => {
-  console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
-  });
-}).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.');
-  console.log(error);
-});
+
+////////////////////////////////////////////////////////////////////Old Request
+// getMovies().then((movies) => {
+//     console.log('Here are all the movies:');
+//     movies.forEach(({title, rating, id}) => {
+//         console.log(`id#${id} - ${title} - rating: ${rating}`);
+//     });
+// }).catch((error) => {
+//     alert('Oh no! Something went wrong.\nCheck the console for details.')
+//     console.log(error);
+// });
+////////////////////////////////////////////////////////////////////
+
+
+getMovies()
+    .then((movies) => {
+        $('#head').html('Here are all the movies:');
+        $('#movieList').html('');
+        movies.forEach(({title, rating, id}) => {
+            $('#movieList').append(`<br>id#${id} - ${title} - rating: ${rating}`);
+        });
+    })
+    .catch((error) => {
+        alert('Oh no! Something went wrong.\nCheck the console for details.');
+        console.log(error);
+    });
+
+const $ = require('jquery');
+
+$(document).ready(
+    console.log('JQuery up and running')
+);
