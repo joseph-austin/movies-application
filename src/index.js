132541d1db95948987c4248e8d978588af0055ad
JustinSOrtega <justinsortega@gmail.com>
2019-07-22 10:23:26 -0500

    added the add func

--- src/index.js
+++ src/index.js
@@ -22,15 +22,19 @@ import getMovies from './api.js';
 //     console.log(error);
 // });
 ////////////////////////////////////////////////////////////////////
-
-
+let ids;
 getMovies()
     .then((movies) => {
+        let ids = movies.length + 1;
+        console.log(ids);
+        $('#userInput').toggleClass('invis');
         $('#head').html('Here are all the movies:');
         $('#movieList').html('');
         movies.forEach(({title, rating, id}) => {
-            $('#movieList').append(`<br>id#${id} - ${title} - rating: ${rating}`);
+            console.log(`id#${id} - ${title} - rating: ${rating}/5`);
+            $('#movieList').append(`<br>id#${id} - ${title} - rating: ${rating}/5`);
         });
+
     })
     .catch((error) => {
         alert('Oh no! Something went wrong.\nCheck the console for details.');
@@ -42,3 +46,43 @@ const $ = require('jquery');
 $(document).ready(
     console.log('JQuery up and running')
 );
+
+///////////User Submissions
+
+$('#submit').on('click', function () {
+    let title = document.getElementById('named').value;
+    // console.log(title);
+    // console.log(document.getElementById('named').value);
+    let rating = document.getElementById('rated').value;
+
+    let movieAdded = {title: title, rating: rating, id: ids};
+    let url = '/api/movies';
+    let options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(movieAdded),
+    };
+    fetch(url, options)
+        .then(/* post was created successfully */)
+        .catch(/* handle errors */);
+});
+
+////////////////////////////////////////////////////////////////////Morning Notes
+
+// GET on /movies - return list of movies
+// POST on /movies - creates a new movie
+// GET on /movies/{id} - return details for movie for specified movie
+// PUT on /movies/{id} - update specified movie
+// DELETE on /movie/{id} - delete specified movie
+//
+// module.exports = {
+//     getMovies: () => {
+//         return fetch('/api/movies')
+//             .then(response => response.json())
+//     },
+//     createdMovie: (request) =>
+// };
+
+//
