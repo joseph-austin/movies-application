981e553440b01e722787ac7265299509bce816a9
JustinSOrtega <justinsortega@gmail.com>
2019-07-22 10:51:26 -0500

    buttons added

--- src/index.js
+++ src/index.js
@@ -29,11 +29,43 @@ getMovies()
         console.log(ids);
         $('#userInput').toggleClass('invis');
         $('#head').html('Here are all the movies:');
-        $('#movieList').html('');
+        $('#load').html('');
         movies.forEach(({title, rating, id}) => {
             console.log(`id#${id} - ${title} - rating: ${rating}/5`);
-            $('#movieList').append(`<br>id#${id} - ${title} - rating: ${rating}/5`);
+            $('#movieList').append(`<br><div>id#${id} - ${title} - rating: ${rating}/5 <button type="submit" class="btn btn-primary edit"><i class="fas fa-edit"></i></button> 
+<button type="submit" class="btn btn-primary delete"><i class="fas fa-trash"></i></div></button>`);
+            $('.edit').click(function () {
+                $('body').css('background-color', 'blue');
+            });
+
         });
+
+        //
+        //     $('#modal').html(`
+        // <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+        //         Edit Movies
+        //     </button>
+        //     <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        //         <div class="modal-dialog" role="document">
+        //         <div class="modal-content">
+        //         <div class="modal-header">
+        //         <h5 class="modal-title" id="exampleModalLabel">Movie Editor</h5>
+        //     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        //         <span aria-hidden="true">&times;</span>
+        //     </button>
+        //     </div>
+        //     <div class="modal-body">
+        // ...
+        // </div>
+        //     <div class="modal-footer">
+        //         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        //         <button type="button" class="btn btn-primary">Save changes</button>
+        //     </div>
+        //     </div>
+        //     </div>
+        //     </div>`
+        // )
+
 
     })
     .catch((error) => {
@@ -68,6 +100,26 @@ $('#submit').on('click', function () {
         .then(/* post was created successfully */)
         .catch(/* handle errors */);
 });
+
+//     let title = document.getElementById('named').value;
+//     // console.log(title);
+//     // console.log(document.getElementById('named').value);
+//     let rating = document.getElementById('rated').value;
+//
+//     let movieAdded = {title: title, rating: rating, id: ids};
+//     let url = '/api/movies';
+//     let options = {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify(movieAdded),
+//     };
+//     fetch(url, options)
+//         .then(/* post was created successfully */)
+//         .catch(/* handle errors */);
+// });
+
 
 ////////////////////////////////////////////////////////////////////Morning Notes
 
